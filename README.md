a5c4db1e463bd35237061fc475c309ea1d12019e
Ethan Welsh <welshej@gmail.com>
2025-11-30 17:02:01 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ❄️AAdvent-Of-Code-2025❄️
+
+AoC 2025 solutions in Haskell.
+
+Run instructions:
+
+```
+# To Run
+stack run 1 input/day01.txt
+
+# To Run Interactively
+ghcid --setup ":set args 1 input/day01.txt" --run="Main.main" --warnings
+```
---
+++ src/Main.hs
@@ -0,0 +1,40 @@
+module Main (main) where
+
+import qualified Day01
+import qualified Day02
+import qualified Day03
+import qualified Day04
+import qualified Day05
+import qualified Day06
+import qualified Day07
+import qualified Day08
+import qualified Day09
+import qualified Day10
+import qualified Day11
+import qualified Day12
+
+import           System.Environment (getArgs)
+
+solvers :: [FilePath -> IO ()]
+solvers =
+    [ Day01.solve
+    , Day01.solve
+    , Day02.solve
+    , Day03.solve
+    , Day04.solve
+    , Day05.solve
+    , Day06.solve
+    , Day07.solve
+    , Day08.solve
+    , Day09.solve
+    , Day10.solve
+    , Day11.solve
+    , Day12.solve
+    ]
+
+main :: IO ()
+main = do
+    [day, filePath] <- getArgs
+    let solver = solvers !! read day
+    putStrLn ""
+    solver filePath
