941a4679ebca7bde6c8554498df686fc8d301479
Ethan Welsh <welshej@gmail.com>
2025-12-13 09:54:15 -0800

    solve part 1

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,24 +1,39 @@
+{-# LANGUAGE BlockArguments #-}
 module Day09 (solve) where
 
 import Text.Megaparsec
 import           Utils.Parsers (Parser)
---import Control.Monad (void)
---import Text.Megaparsec.Char (string, char, newline)
+import Text.Megaparsec.Char (char, eol)
+import qualified Text.Megaparsec.Char.Lexer as L
+import Data.List (tails, sortBy)
 
-type Input = String
+type Point = (Int, Int)
+type Input = [Point]
+
+parsePoint :: Parser Point
+parsePoint = (,) <$> L.decimal <* char ',' <*> L.decimal
 
 parseInput :: Parser Input
-parseInput = error "TODO"
+parseInput = parsePoint `sepEndBy` eol
+
+allPairs :: [a] -> [(a, a)]
+allPairs xs = [(x, y) | (x:ys) <- tails xs, y <- ys]
+
+area :: Point -> Point -> Int
+area (x1, y1) (x2, y2) = (abs (x1 - x2) + 1) * (abs (y1 - y2) + 1)
 
 part1 :: Input -> IO ()
 part1 input = do
   putStr "Part 1: "
-  print input
+  let pairs = allPairs input
+  let sorted = sortBy (flip \(a,b) (c,d) -> compare (area a b) (area c d)) pairs
+  let topArea = uncurry area (head sorted)
+  print topArea
+  --print $ area $ head sorted
 
 part2 :: Input -> IO ()
-part2 input = do
+part2 _ = do
   putStr "Part 2: "
-  print input
 
 solve :: FilePath -> IO ()
 solve filePath = do
